ba76475f2aae93823da7f7b12b00026eb371ac3e
Justin Driscoll <justin@driscolldev.com>
2011-12-09 14:00:14 -0500

    Fix callback block return types and use typedefs to declare them

--- Classes/JCDHTTPConnection.h
+++ Classes/JCDHTTPConnection.h
@@ -5,6 +5,11 @@
 
 #import <Foundation/Foundation.h>
 
+typedef void (^OnSuccess) (NSHTTPURLResponse *, NSString *);
+typedef void (^OnFailure) (NSHTTPURLResponse *, NSString *, NSError *);
+typedef void (^OnDidSendData) (NSInteger, NSInteger, NSInteger);
+
+
 @interface JCDHTTPConnection : NSObject
 
 @property (strong, nonatomic) NSURLRequest *request;
@@ -12,13 +17,13 @@
 @property (strong, nonatomic) NSMutableData *data;
 @property (readonly) NSString *body;
 
-@property (copy) id (^onSuccess)(NSHTTPURLResponse *, NSString *);
-@property (copy) id (^onFailure)(NSHTTPURLResponse *, NSString *, NSError *);
-@property (copy) id (^onDidSendData)(NSInteger, NSInteger, NSInteger);
+@property (copy, nonatomic) OnSuccess onSuccess;
+@property (copy, nonatomic) OnFailure onFailure;
+@property (copy, nonatomic) OnDidSendData onDidSendData;
 
 - (id)initWithRequest:(NSURLRequest *)urlRequest;
-- (BOOL)executeRequestOnSuccess:(void (^)(NSHTTPURLResponse *, NSString *))onSuccessBlock
-                        failure:(void (^)(NSHTTPURLResponse *, NSString *, NSError *))onFailureBlock
-                    didSendData:(void (^)(NSInteger, NSInteger, NSInteger))onDidSendDataBlock;
+- (BOOL)executeRequestOnSuccess:(OnSuccess)onSuccessBlock
+                        failure:(OnFailure)onFailureBlock
+                    didSendData:(OnDidSendData)onDidSendDataBlock;
 
 @end
